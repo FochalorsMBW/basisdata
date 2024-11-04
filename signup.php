b9d72167896840f7032b04a3dd5121021239ae30
FochalorsMBW <146565204+FochalorsMBW@users.noreply.github.com>
2024-11-04 22:54:39 +0700

    signup.php

--- signup.php
+++ signup.php
@@ -1,15 +1,19 @@
 <?php
 include 'koneksi.php'; // Mengimpor koneksi ke database
-$nama = $_POST['nama'];
-$email = $_POST['email'];
-$password = $_POST['kata_sandi'];
 
 
-$query_sql = "INSERT INTO registrasi (nama, email, kata_sandi) VALUES ('$nama', '$email', '$password')";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $password = $_POST['kata_sandi'];
+
+    $query_sql = "INSERT INTO registrasi (nama, email, kata_sandi) VALUES ('$nama', '$email', '$password')";
 
 if (mysqli_query($conn, $query_sql)) {
     header("Location: index.html");
 } else {
     echo "Pendaftaran Gagal : " . mysqli_error($conn);
 }
+}
+
 ?>
